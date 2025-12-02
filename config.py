caf65ed1b8e9af6c6795e73652bbeb70fed3729b
JiejieLiM <163673772+JiejieLiM@users.noreply.github.com>
2025-12-02 11:23:53 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cvnf8dv6pwgf.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "jjlim-s3-bucket"
+customregion = "us-east-1"
+
